4b5c8cc10c7d2fe0c0fba43094e8a0303ed07b01
ZmeelovNotebok <Zmeelov7598@yandex.ru>
2022-10-20 21:49:21 +0600

    Factorial Recursiv Finish

---
+++ Factorial/Program.cs
@@ -0,0 +1,15 @@
+﻿int Factorial(int n)
+{
+    int result = 1;
+    if(n == 0)
+    {
+        return result;
+    }
+    else
+    {
+        return result = n * Factorial(n-1);
+    }
+}
+
+int number = 5;
+Console.WriteLine(Factorial(number));
